00877910d7480fc565d1d0452b1cd685382b5065
Yuta Takayama <yprogdbcs@icloud.com>
2020-08-24 14:13:01 +0900

    DDL作成

--- docker/pg/1-createTable.sql
+++ docker/pg/1-createTable.sql
@@ -1,11 +1,48 @@
 create table users (
   id serial primary key,
-  name varchar not null,
-  age integer not null
+  name varchar(30) not null,
+  mail varchar(50) not null,
+  filterlevel smallint
 );
 
-insert into users(name, age) values
-  ('ichigo.chocomint', 99),
-  ('banana.chocomint', 98),
-  ('pinapple.chocomint', 97)
+create table friend (
+  id serial primary key,
+  userid INTEGER not null,
+  friendid INTEGER not null,
+  name varchar(30) not null,
+  latestemolog varchar(30)
+);
+
+create table emolog (
+  id serial primary key,
+  userid INTEGER not null,
+  friendid INTEGER not null,
+  create_at timestamp,
+  contents varchar(30)
+);
+
+create table talk (
+  id serial primary key,
+  userid INTEGER not null,
+  friendid INTEGER not null,
+  contents varchar(30)
+  create_at timestamp,
+);
+
+create table requestfriend (
+  id serial primary key,
+  userid INTEGER not null,
+  requestfriendid INTEGER not null,
+  contents varchar(30)
+  create_at timestamp,
+);
+
+
+
+
+insert into users(name, mail, filterlevel) values
+  ('ichigo.chocomint', 'hoge@hoge.com', 1),
+  ('banana.chocomint', 'hogehoge@hoge.com', 2),
+  ('pinapple.chocomint', 'hogehogehoge@hoge.com', 3)
 ;
+
