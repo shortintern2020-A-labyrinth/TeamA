b4a3a7cfefe4a924300d2636f0fa80b821a67585
Masahito-I <mwgptl153@gmail.com>
2020-08-24 14:51:14 +0900

    commit

--- docker/pg/1-createTable.sql
+++ docker/pg/1-createTable.sql
@@ -1,16 +1,25 @@
+/*テーブル作成*/
 create table users (
   id serial primary key,
   name varchar not null,
   age integer not null
 );
 
+/*
 create table emojicode (
-  keyword serial primary key,
-  emoji_code 
-);
+  keyword Text not null,
+  emoji_code  Text not null
+);*/
 
+/*初期データ挿入*/
 insert into users(name, age) values
   ('ichigo.chocomint', 99),
   ('banana.chocomint', 98),
   ('pinapple.chocomint', 97)
 ;
+
+/*
+insert into emojicode(keyword, emoji_code) values
+  ("楽しい", "&#x1f600;"),
+  ("悲しい", "&#x1f923;"),
+;初期データ挿入*/
