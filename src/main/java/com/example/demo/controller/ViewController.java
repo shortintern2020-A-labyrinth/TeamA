3fdaed860eb2a2e15db1936435e1ccb4080d4441
nawta <wes904@icloud.com>
2020-08-24 16:53:02 +0900

    implemented search tweets of specific user

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -7,10 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import twitter4j.*;
 
 @RestController
@@ -29,19 +27,43 @@ public class ViewController {
     }
 
     @RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
-    public String read(@PathVariable String id) {
+    public String read(@PathVariable("id") String id) {
         List<Map<String, Object>> list;
         list = jdbcTemplate.queryForList("select * from users where id = ?", id);
         return list.toString();
     }
 
     @RequestMapping(path = "/search_keyword", method = RequestMethod.GET)
-    public String search_keyowrd() throws TwitterException {
+    public String search_keyowrd(@RequestParam(name = "searchword", defaultValue = "チンチン") String searchword) throws TwitterException {
         // 初期化
         Twitter twitter = new TwitterFactory().getInstance();
-        Query query = new Query("うんこ");
+        Query query = new Query(searchword);
 
         QueryResult result = twitter.search(query);
+
+        // for debug. show tweet results on CLI
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
+
+        return "a";
+    }
+
+    @RequestMapping(path = "/search_user", method = RequestMethod.GET)
+    public String search_user(@RequestParam(name = "username", defaultValue = "CNN") String username,
+                              @RequestParam(name = "tweet_id", defaultValue = "1000000") long tweet_id
+    ) throws TwitterException {
+        // 初期化
+        Twitter twitter = new TwitterFactory().getInstance();
+        Query query = new Query();
+
+        //ユーザーネームで指定.sinceId以降のtweetから15個取ってくる.最新のツイートから時系列順に取ってくるっぽい。
+        query.setQuery("from:" + username);
+        query.setSinceId(tweet_id);
+
+        QueryResult result = twitter.search(query);
+
+        //for debug on CLI.
         for (Status status : result.getTweets()) {
             System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
         }
