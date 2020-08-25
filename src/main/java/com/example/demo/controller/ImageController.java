7edbd048fd923613f81fcf284e205e7d1993595a
tsukasa2 <t.nakamura@posl.ait.kyushu-u.ac.jp>
2020-08-25 14:08:36 +0900

    classify local image

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controller;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.natural_language_understanding.v1.model.CategoriesResult;
+import com.ibm.watson.natural_language_understanding.v1.model.Features;
 import com.ibm.watson.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.visual_recognition.v3.model.*;
 import org.slf4j.Logger;
@@ -13,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.io.File;
 
 @RestController
 @RequestMapping(path = "/Image")
@@ -23,18 +29,21 @@ public class ImageController {
     JdbcTemplate jdbcTemplate;
 
     @RequestMapping(path = "/test", method = RequestMethod.GET)
-    public String test() {
+    public String test(
+            @RequestParam(name = "img_path", defaultValue = "/Users/NakamuraTsukasa/Desktop/633.png") String img_path
+    ) throws FileNotFoundException {
+//        String url = "https://zukan.pokemon.co.jp/zukan-api/up/images/index/94b9fb82b38847b83a8041e9a78989ce.png";
+//        String img_path = "/Users/NakamuraTsukasa/Desktop/634.png";
+
         IamAuthenticator authenticator = new IamAuthenticator("8nwN0eKp7eZ73So4DLdPndp_yv-vtlI27pN1wK2TjVg1");
         VisualRecognition visualRecognition = new VisualRecognition("2019-07-12", authenticator);
-//        visualRecognition.setServiceUrl("https://zukan.pokemon.co.jp/zukan-api/up/images/index/");
 
-        System.out.println("Classify an image");
         ClassifyOptions options = new ClassifyOptions.Builder()
-                .url("https://zukan.pokemon.co.jp/zukan-api/up/images/index/94b9fb82b38847b83a8041e9a78989ce.png")
+                //.url(url)
+                .imagesFile(new File(img_path))
                 .build();
         ClassifiedImages result = visualRecognition.classify(options).execute().getResult();
         System.out.println(result);
-        return "asdfg";
+        return result.toString();
     }
-
 }
