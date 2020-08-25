0fa45bc729cbae451a252226752f5edf40cdea1a
Masahito-I <mwgptl153@gmail.com>
2020-08-26 03:55:33 +0900

    画面デザインの修正、ごり押しのデータベースアクセスによるデータ取得とその表示

--- docker/pg/1-createTable.sql
+++ docker/pg/1-createTable.sql
@@ -19,38 +19,29 @@ create table emolog (
   userid INTEGER not null,
   friendid INTEGER not null,
   create_at timestamp,
-  contents varchar(30)
+  contents varchar(100)
 );
 
 create table talk (
   id serial primary key,
   userid INTEGER not null,
   friendid INTEGER not null,
-  contents varchar(30)
-  create_at timestamp,
+  contents varchar(30),
+  create_at timestamp
 );
 
 create table requestfriend (
   id serial primary key,
   userid INTEGER not null,
   requestfriendid INTEGER not null,
-  contents varchar(30)
-  create_at timestamp,
+  contents varchar(30),
+  create_at timestamp
 );
 
-/*
 create table emojicode (
-<<<<<<< HEAD
   keyword Text not null,
   emoji_code  Text not null
-);*/
-
-/*初期データ挿入*/
-=======
-  keyword serial primary key,
-  emoji_code
 );
-
 
 insert into users(name, mail, filterlevel) values
   ('ichigo.chocomint', 'hoge@hoge.com', 1),
@@ -58,19 +49,23 @@ insert into users(name, mail, filterlevel) values
   ('pinapple.chocomint', 'hogehogehoge@hoge.com', 3)
 ;
 
-
->>>>>>> aee0b171f30a59de419941fd6741b58d34595557
-insert into users(name, age) values
-  ('ichigo.chocomint', 99),
-  ('banana.chocomint', 98),
-  ('pinapple.chocomint', 97)
-;
-<<<<<<< HEAD
-
-/*
 insert into emojicode(keyword, emoji_code) values
-  ("楽しい", "&#x1f600;"),
-  ("悲しい", "&#x1f923;"),
-;初期データ挿入*/
-=======
->>>>>>> aee0b171f30a59de419941fd6741b58d34595557
+  ('radio', ':radio:'),
+  ('movies', ':movie_camera:'),
+  ('bowling',':bowling:'),
+  ('bank',':bank:')
+;
+
+insert into talk(userid, friendid,contents,create_at) values
+  (100, 101,'Hello World !', current_timestamp),
+  (101,100, 'Hi !', current_timestamp),
+  (101,100,'See you.','2020-08-25 10:23:23')
+;
+
+insert into emolog(userid,friendid,create_at,contents) values
+  (101,100,'2020-08-25 10:23:23',':grinning: :smiley: :wink: :radio: :credit_card:'),
+  (100,101,'2020-08-25 10:23:23',':wink: :radio: :credit_card: :grinning: :smiley:')
+;
+
+
+
---
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -0,0 +1,142 @@
+package com.example.demo.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import java.util.List;
+import java.util.Map;
+
+//import java.util.Date;
+import com.vdurmont.emoji.EmojiParser;
+
+import org.springframework.stereotype.Controller;
+
+//chatページの表示
+@Controller
+@RequestMapping(path = "/")
+public class ChatController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(path = "/chat", method = RequestMethod.GET)
+    public String chat(Model model){
+        //日付
+        //Date date = new Date();
+        /* */
+        String date = "8月18日";
+        model.addAttribute("date", date);
+
+        //絵文字
+        //データを受け取る
+        int u_id = 100;
+        int f_id = 101;
+        //SELECT contents FROM emolog WHERE userid== AND friendid AND create_at==date;
+        List<Map<String, Object>> emojilist;
+        emojilist = jdbcTemplate.queryForList("SELECT * FROM emolog");
+        String MyEmolog ="";
+        String FriendEmolog ="";
+        for(Map<String, Object> map : emojilist) {
+            if(u_id == (Integer)map.get("userid") && f_id == (Integer)map.get("friendid") ){ 
+                //本当は一日分なので日付も欲しいですが
+                MyEmolog += map.get("contents");
+            }else if(f_id == (Integer)map.get("userid") && u_id == (Integer)map.get("friendid")){
+                FriendEmolog +=map.get("contents");
+             }
+        }
+
+        //絵文字の表示部分
+        //相手側
+        //String text = "An 😀awesome 😃string with a few 😉emojis!";
+        //String text = ":grinning: :smiley: :wink: :radio: :credit_card: ";
+        String text =FriendEmolog; 
+        text = EmojiParser.parseToUnicode(text);
+        model.addAttribute("FriendEmolog", text);
+        //自分側
+        text=MyEmolog;
+        text = EmojiParser.parseToUnicode(text);
+        model.addAttribute("MyEmolog", text);
+
+
+         //チャット部分の表示
+         List<Map<String, Object>> list;
+         //list = jdbcTemplate.queryForList("select * from users");
+         list = jdbcTemplate.queryForList("SELECT * FROM talk");
+         
+         String MyMessage ="";
+         String FriendMessage ="";
+         for(Map<String, Object> map : list) {
+             if(u_id == (Integer)map.get("userid") && f_id == (Integer)map.get("friendid") ){ 
+                 //本当は一日分なので日付も欲しいですが
+                MyMessage += map.get("contents");
+             }else if(f_id == (Integer)map.get("userid") && u_id == (Integer)map.get("friendid")){
+                FriendMessage +=map.get("contents");
+             }
+         }
+         model.addAttribute("MyChatMessage", MyMessage);
+         model.addAttribute("FriendChatMessage", FriendMessage);
+         
+        return "chat";
+    }
+
+    /*自分用
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String twitter_connect(Model model){
+        model.addAttribute("MyEmolog", "Hello Springboot");
+        return "chat";
+    }*/
+
+
+    //テキストボックス入力
+    /*@RequestMapping(value = "/chat", method = RequestMethod.GET)
+    public String TextBox(Model model){
+        // 空のフォームオブジェクトをModelに設定
+		model.addAttribute("MessageBox", new Message());
+        // 遷移先を返す(この場合はinput.htmlが遷移先となる)
+        return "chat";
+    }*/
+
+    /*@RequestMapping(value = "/chat", method = RequestMethod.POST)
+	public String confirm(@ModelAttribute("MessageBox") Message form,Model model) {
+        // 空のフォームオブジェクトをModelに設定
+        model.addAttribute("MessageBox", new Message());
+        
+        // 遷移先を返す(この場合はconfirm.htmlが遷移先となる)
+		return "chat";
+	}*/
+
+
+    //private static final Logger LOG = LoggerFactory.getLogger(ViewController.class);
+/*
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    
+    @RequestMapping(path = "/users", method = RequestMethod.GET)
+    public String index() {
+        List<Map<String, Object>> list;
+        list = jdbcTemplate.queryForList("select * from users");
+        return list.toString();
+    }
+
+    @RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
+    public String read(@PathVariable("id") String id) {
+        List<Map<String, Object>> list;
+        list = jdbcTemplate.queryForList("select * from users where id = ?", id);
+        return list.toString();
+    }
+
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String twitter_connect() throws Exception{
+        return "";
+    }*/
+
+}
