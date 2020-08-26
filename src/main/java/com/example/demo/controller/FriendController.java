4f9a620c6e95a579837dd98a9891068d48ca15b5
Yuta Takayama <yprogdbcs@icloud.com>
2020-08-26 14:07:12 +0900

    #23 friendlistのbackend実装

---
+++ src/main/java/com/example/demo/controller/FriendController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Friend;
+import com.example.demo.service.FriendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/friendlist")
+public class FriendController {
+
+    @Autowired
+    FriendService service;
+
+    @RequestMapping(value="/{userid}",method=RequestMethod.GET)
+    public String selectAll(@PathVariable("userid") String userid, Model model) {
+        int userId = Integer.parseInt(userid);
+        List<Friend> friends = service.selectAll(userId);
+        model.addAttribute("friends", friends);
+        return "friendlist.html";
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/FriendService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Friend;
+import com.example.demo.repository.FriendRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FriendService {
+
+    @Autowired
+    FriendRepository repository;
+
+    public List<Friend> selectAll(int user){
+        return repository.selectAll(user);
+    }
+}
