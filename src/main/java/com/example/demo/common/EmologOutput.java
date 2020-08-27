f0045cf8015458acea7b6a94e9a622b669719946
Yuta Takayama <yprogdbcs@icloud.com>
2020-08-27 09:55:27 +0900

    twitter系の関数まとめファイル

--- src/main/java/com/example/demo/common/EmologOutput.java
+++ src/main/java/com/example/demo/common/EmologOutput.java
@@ -8,7 +8,6 @@ import com.ibm.watson.visual_recognition.v3.model.*;
 import com.vdurmont.emoji.Emoji;
 import com.vdurmont.emoji.EmojiManager;
 import org.apache.commons.lang3.StringUtils;
-import org.seasar.doma.internal.util.StringUtil;
 import twitter4j.*;
 
 import java.io.FileNotFoundException;
@@ -23,13 +22,13 @@ public class EmologOutput {
      //input:  long tweet_id
      //output: Stringlist keywords
      */
-    public List<String> collect_NLU_keywords_from_tweets(String username, long tweet_id
+    public List<String> collectNLUkeywords(String username, long tweet_id
     ) throws TwitterException {
-        QueryResult result = search_user(username, tweet_id);
+        QueryResult result = querySearch(username, tweet_id);
         ArrayList<String> NLU_results = new ArrayList<>();
 
         for (Status status : result.getTweets()) {
-            String tmp = NLU(status.getText());
+            String tmp = calcNLUKeyword(status.getText());
             if(!StringUtils.isBlank(tmp)) {
 //                System.out.println(tmp);
                 NLU_results.add(tmp);
@@ -44,16 +43,16 @@ public class EmologOutput {
      //input:  long tweet_id
      //output: StringList keywords
      */
-    public List<String> collect_image_keywords_from_tweets(String username, long tweet_id
+    public List<String> collectImagekeywords(String username, long tweet_id
     ) throws Exception {
-        QueryResult result = search_user(username, tweet_id);
+        QueryResult result = querySearch(username, tweet_id);
         ArrayList<String> imageprocessing_results = new ArrayList<>();
 
         for (Status status : result.getTweets()) {
             if(status.getMediaEntities().length > 0) {
 //                System.out.println(status.getMediaEntities()[0].getMediaURL());
 
-                String tmp = imageProcessing(status.getMediaEntities()[0].getMediaURL());
+                String tmp = calcImageKeyword(status.getMediaEntities()[0].getMediaURL());
                 if(!StringUtils.isBlank(tmp)) {
 //                    System.out.println(tmp);
                     imageprocessing_results.add(tmp);
@@ -72,8 +71,7 @@ public class EmologOutput {
      //input:  long tweet_id
      //output: QueryResult
      */
-    //TODO: TwitterControllerやNLUControllerから共通部分を分離して別クラスとして保持。
-    private QueryResult search_user(String username, long tweet_id) throws TwitterException {
+    private QueryResult querySearch(String username, long tweet_id) throws TwitterException {
         // 初期化
         Twitter twitter = new TwitterFactory().getInstance();
         Query query = new Query();
@@ -90,7 +88,7 @@ public class EmologOutput {
      //input:  String text
      //output: String keyword
      */
-    private String NLU(String text) {
+    private String calcNLUKeyword(String text) {
         IamAuthenticator authenticator = new IamAuthenticator("fVfaYMA7tCh4zInWBhTBb5t69xQryK7ObKl42nampynG");
         NaturalLanguageUnderstanding naturalLanguageUnderstanding = new NaturalLanguageUnderstanding("2019-07-12", authenticator);
         naturalLanguageUnderstanding.setServiceUrl("https://api.jp-tok.natural-language-understanding.watson.cloud.ibm.com/instances/ac3df365-93f6-4255-beb8-620d66041251");
@@ -128,7 +126,7 @@ public class EmologOutput {
      //input:  String imageUrl
      //output: String keyword
      */
-    private String imageProcessing(String url) throws FileNotFoundException {
+    private String calcImageKeyword(String url) throws FileNotFoundException {
         IamAuthenticator authenticator = new IamAuthenticator("8nwN0eKp7eZ73So4DLdPndp_yv-vtlI27pN1wK2TjVg1");
         VisualRecognition visualRecognition = new VisualRecognition("2019-07-12", authenticator);
 
@@ -151,7 +149,7 @@ public class EmologOutput {
      //input:  String keyword
      //output: emoji (Unicode)
      */
-    public String convert(String keyword) {
+    public String convertEmoji(String keyword) {
         keyword = keyword.replace(" ", "_"); //空白を_に置換
         keyword = ":" + keyword + ":";
         Emoji emoji;
@@ -171,34 +169,33 @@ public class EmologOutput {
      //input:  (username, tweet_id)
      //output: ArrayList keywords
      */
-    public List<String> get_NLU_keywords(String username, long tweet_id) throws TwitterException {
-        QueryResult result = search_user(username, tweet_id);
+    public List<String> calcNLUKeywords(String username, long tweet_id) throws TwitterException {
+        QueryResult result = querySearch(username, tweet_id);
         ArrayList<String> keywords = new ArrayList<>();
 
         for (Status status : result.getTweets()) {
-            String tmp = NLU(status.getText());
+            String tmp = calcNLUKeyword(status.getText());
             if(tmp != null) {
 //                System.out.println(tmp);
                 keywords.add(tmp);
             }
         }
         return keywords;
-        //TODO: 整形する必要あるかも？
     }
 
     /** image to keyword
     //input:  (username, tweet_id)
     //output: ArrayList keywords
      */
-    public List<String> get_image_keywords(String username, long tweet_id) throws Exception {
-        QueryResult result = search_user(username, tweet_id);
+    public List<String> calcImageKeywords(String username, long tweet_id) throws Exception {
+        QueryResult result = querySearch(username, tweet_id);
         ArrayList<String> keywords = new ArrayList<>();
 
         for (Status status : result.getTweets()) {
             if(status.getMediaEntities().length > 0) {
 //                System.out.println(status.getMediaEntities()[0].getMediaURL());
 
-                String tmp = imageProcessing(status.getMediaEntities()[0].getMediaURL());
+                String tmp = calcImageKeyword(status.getMediaEntities()[0].getMediaURL());
                 if (tmp != null) {
 //                    System.out.println(tmp);
                     keywords.add(tmp);
@@ -210,8 +207,4 @@ public class EmologOutput {
 //                ["call center", "people", "newsreader"]みたいなんがくる。
 
     }
-
-
-
-
 }
